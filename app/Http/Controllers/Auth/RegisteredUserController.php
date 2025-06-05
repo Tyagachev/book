4a916aa5531b3627976043c3d77dd9fb4016067b
Anton <570uz@mail.ru>
2025-06-05 18:37:32 +0300

    Временное добавление роли юзера при регистрации

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -41,6 +42,8 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        $role = Role::query()->find(2);
+        $user->roles()->attach($role);
 
         event(new Registered($user));
 
